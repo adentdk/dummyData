8538e78b95061173c3981557790fb910c37a8d2b
aden trisna daud kurnia <adentrisnadaudkurnia@gmail.com>
2019-10-08 16:32:42 +0700

    handling error and add indeRoute

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-require("dotenv").config
+require("dotenv").config;
+const createError =  require("http-errors");
 const express = require("express");
 const app = express();
+const { PORT } = process.env;
+const port = PORT || 3000;
 
-const { PORT } = process.env
+const indexRoute =  require("./routes/index");
 
-const port = PORT || 3000
+app.use("/",indexRoute);
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        "status": 200,
-        "message": "OK",
-        "data" : {
-            "text": "Welcome to dummydata API" 
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500; 
+    const valueText = status === 404 ? "No route match" : "Internal server error";
+    res.status(status).send({
+        status: status,
+        message: "error",
+        value: {
+            text: valueText
         }
     })
 });
+
 
 app.listen(port, () => {
     console.log("server is running")
